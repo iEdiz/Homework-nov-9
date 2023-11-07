d9b034471a0fa6478e766782efc995a7af3dca69
Edijs Mezits <edijs.mezits@gmail.com>
2023-11-07 19:29:48 +0200

    Completed 7 more pages of tasks

--- html_ts_boilerplate-master/src/scripts.ts
+++ html_ts_boilerplate-master/src/scripts.ts
@@ -1,3 +1,5 @@
+import { testResultsProcessor } from '../jest.config';
+
 const sum = (a: number, b: number): number => a + b;
 
 console.log(sum(1, 2));
@@ -191,3 +193,151 @@ console.log(findAnyNumber(38));
 console.log(findAnyNumber(7));
 console.log(findAnyNumber(127));
 console.log(findAnyNumber(2000));
+
+const isDivisable = (x: number, y: number) => {
+  while (x % y !== 0) {
+    x++;
+  }
+  return x;
+};
+
+console.log(isDivisable(1, 23));
+console.log(isDivisable(23, 23));
+console.log(isDivisable(7, 3));
+console.log(isDivisable(-5, 7));
+
+const twoString = (a: string, b: string) => a.replace(/(?=(...)+$)/g, b);
+
+console.log(twoString('1234567', '.'));
+console.log(twoString('abcde', '$'));
+console.log(twoString('zxyzxyzxyzxyzxyz', 'w'));
+
+const nextLetter = (a: string) => {
+  let result = '';
+  for (let i = 0; i < a.length; i++) {
+    let char = a[i];
+    if (char >= 'a' && char <= 'z') {
+      char = String.fromCharCode(((char.charCodeAt(0) - 97 + 1) % 26) + 97);
+    }
+    result += char;
+  }
+  return result;
+};
+
+console.log(nextLetter('bnchmf'));
+console.log(nextLetter('bgddrd'));
+console.log(nextLetter('sdrshmf'));
+
+const arrayReturn = (a: number[], b: number) => a[b - 1];
+
+console.log(arrayReturn([1, 2, 3, 4, 5], 3));
+console.log(arrayReturn([10, 9, 8, 7, 6], 5));
+console.log(arrayReturn([7, 2, 1, 6, 3], 1));
+
+const removeArray = (a: number[]) => a.slice(3);
+
+console.log(removeArray([1, 2, 3, 4]));
+console.log(removeArray([5, 4, 3, 2, 1, 0]));
+console.log(removeArray([99, 1, 1]));
+
+const lastThreeArray = (a: number[]) => {
+  const secondArray = a.slice(-3);
+  return secondArray;
+};
+
+console.log(lastThreeArray([1, 2, 3, 4]));
+console.log(lastThreeArray([5, 4, 3, 2, 1, 0]));
+console.log(lastThreeArray([99, 1, 1]));
+
+const firstThreeArray = (a: number[]) => {
+  const secondArray = a.slice(0, 3);
+  return secondArray;
+};
+
+console.log(firstThreeArray([1, 2, 3, 4]));
+console.log(firstThreeArray([5, 4, 3, 2, 1, 0]));
+console.log(firstThreeArray([99, 1, 1]));
+
+const lastElements = (a: number[], n: number) => {
+  const secondArray = a.slice(-n);
+  return secondArray;
+};
+
+console.log(lastElements([1, 2, 3, 4, 5], 2));
+console.log(lastElements([1, 2, 3], 6));
+console.log(lastElements([1, 2, 3, 4, 5, 6, 7, 8], 3));
+
+const clearArray = (a: any, b: any) => {
+  const unfilteredArray = a.filter((number: any) => number !== b);
+  return unfilteredArray;
+};
+
+console.log(clearArray([1, 2, 3], 2));
+console.log(clearArray([1, 2, '2'], '2'));
+console.log(clearArray([false, '2', 1], false));
+console.log(clearArray([1, 2, '2', 1], 1));
+
+const numberOfElementsArray = (a: number[]) => a.length;
+
+console.log(numberOfElementsArray([1, 2, 2, 4]));
+console.log(numberOfElementsArray([9, 9, 9]));
+console.log(numberOfElementsArray([4, 3, 2, 1, 0]));
+
+const negativeValues = (a: number[]) => {
+  const negativesCount = a.filter((value) => value < 0);
+  return negativesCount.length;
+};
+
+console.log(negativeValues([1, -2, 2, -4]));
+console.log(negativeValues([0, 9, 1]));
+console.log(negativeValues([4, -3, 2, 1, 0]));
+
+const sortArray = (a: number[]) => a.sort().reverse();
+
+console.log(sortArray([1, 3, 2]));
+console.log(sortArray([4, 2, 3, 1]));
+
+const stringSortArray = (a: string[]) => a.sort();
+
+console.log(stringSortArray(['b', 'c', 'd', 'a']));
+console.log(stringSortArray(['z', 'c', 'd', 'a', 'y', 'a', 'w']));
+
+const averageNumbersArray = (a: number[]) => a.reduce((a, b) => a + b, 0) / a.length;
+
+console.log(averageNumbersArray([10, 100, 40]));
+console.log(averageNumbersArray([10, 100, 1000]));
+console.log(averageNumbersArray([-50, 0, 50, 200]));
+
+const longestString = (a: string[]) => {
+  let longest = '';
+  for (const str of a) {
+    if (str.length > longest.length) {
+      longest = str;
+    }
+  }
+  return longest;
+};
+
+console.log(longestString(['help', 'me']));
+console.log(longestString(['I', 'need', 'candy']));
+
+const equalElements = (a: any[]) => {
+  const referenceElements = a[0];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== referenceElements) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(equalElements([true, true, true, true]));
+console.log(equalElements(['test', 'test', 'test']));
+console.log(equalElements([1, 1, 1, 2]));
+console.log(equalElements(['10', 10, 10, 10]));
+
+const multipleArrays = (...arrays: any[]) => [].concat(...arrays);
+
+console.log(multipleArrays([1, 2, 3], [4, 5, 6]));
+console.log(multipleArrays(['a', 'b', 'c'], [4, 5, 6]));
+console.log(multipleArrays([true, true], [1, 2], ['a', 'b']));
