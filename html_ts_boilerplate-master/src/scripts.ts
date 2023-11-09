b2bb25ee236e4ad6710dff1639a493c507685745
Edijs Mezits <edijs.mezits@gmail.com>
2023-11-09 13:58:17 +0200

    Added instructions to every task

--- html_ts_boilerplate-master/src/scripts.ts
+++ html_ts_boilerplate-master/src/scripts.ts
@@ -1,4 +1,6 @@
-
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
 
 const sum = (a: number, b: number): number => a + b;
 
@@ -6,13 +8,19 @@ console.log(sum(1, 2));
 console.log(sum(1, 10));
 console.log(sum(99, 1));
 
-const functionValue = (a: any) => typeof a;
+// Write a function that takes a value as argument
+// Return the type of the value
+
+const functionValue = (a: any): any => typeof a;
 
 console.log(functionValue(1));
 console.log(functionValue(false));
 console.log(functionValue(null));
 console.log(functionValue('string'));
 console.log(functionValue(['array']));
+
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
 
 const typeValue = (a: any, b: any) => {
   if (a === b && typeof a === typeof b) {
@@ -26,11 +34,18 @@ console.log(typeValue(3, 3));
 console.log(typeValue(1, '1'));
 console.log(typeValue('10', '10'));
 
+// Write a function that takes a string (a) and a number (n) as arguments
+// Return the nth character of 'a'
+
 const nthChar = (a: string, n: number) => a.charAt(n - 1);
 
 console.log(nthChar('abcd', 1));
 console.log(nthChar('zyxbwpl', 5));
 console.log(nthChar('gfedcba', 3));
+
+// Write a function that takes a string (a) as argument
+// Remove the first 3 characters of a
+// Return the result
 
 const removeChar = (a: string) => a.substring(3);
 
@@ -38,17 +53,29 @@ console.log(removeChar('abcdefg'));
 console.log(removeChar('1234'));
 console.log(removeChar('fgedcba'));
 
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+
 const removeLast = (a: string) => a.substring(a.length - 3);
 
 console.log(removeLast('abcdefg'));
 console.log(removeLast('1234'));
 console.log(removeLast('fgedcba'));
 
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result
+
 const firstThree = (a: string) => a.substring(0, 3);
 
 console.log(firstThree('abcdefg'));
 console.log(firstThree('1234'));
 console.log(firstThree('fgedcba'));
+
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
 
 const firstHalf = (a: string) => {
   if (a.length % 2 === 0) {
@@ -61,11 +88,18 @@ console.log(firstHalf('abcdefgh'));
 console.log(firstHalf('1234'));
 console.log(firstHalf('gedcba'));
 
+// Write a function that takes a string (a) as argument
+// Remove the last 3 characters of a
+// Return the result
+
 const lastThree = (a: string) => a.slice(0, -3);
 
 console.log(lastThree('abcdefg'));
 console.log(lastThree('1234'));
 console.log(lastThree('fgedcba'));
+
+// Write a function that takes two numbers (a and b) as argument
+// Return b percent of a
 
 const percentNumbers = (a: number, b: number) => (b / 100) * a;
 
@@ -73,11 +107,23 @@ console.log(percentNumbers(100, 50));
 console.log(percentNumbers(10, 1));
 console.log(percentNumbers(500, 25));
 
-const sixNumbers = (a: number, b: number, c: number, d: number, e: number, f: number) => (((a + b) - c) * d / e) ** f;
+// Write a function that takes 6 values (a,b,c,d,e,f) as arguments
+// Sum a and b
+// Then substract by c
+// Then multiply by d and divide by e
+// Finally raise to the power of f and return the result
+// Tip: mind the order
+
+const sixNumbers = (a: number, b: number, c: number, d: number, e: number, f: number) =>
+(((a + b) - c) * d / e) ** f;
 
 console.log(sixNumbers(6, 5, 4, 3, 2, 1));
 console.log(sixNumbers(6, 2, 1, 4, 2, 3));
 console.log(sixNumbers(2, 3, 6, 4, 2, 3));
+
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
 
 const evenNumber = (a: number) => {
   if (a % 2 === 0) {
@@ -91,12 +137,19 @@ console.log(evenNumber(-4));
 console.log(evenNumber(5));
 console.log(evenNumber(-111));
 
+// Write a function that takes two strings (a and b) as arguments
+// Return the number of times a occurs in b
+
 const occurance = (a: string, b: string) => b.split(a).length - 1;
 
 console.log(occurance('m', 'how many times does the character occur in this sentence?'));
 console.log(occurance('h', 'how many times does the character occur in this sentence?'));
 console.log(occurance('?', 'how many times does the character occur in this sentence?'));
 console.log(occurance('z', 'how many times does the character occur in this sentence?'));
+
+// Write a function that takes a number (a) as argument
+// If a is a whole number (has no decimal place), return true
+// Otherwise, return false
 
 const wholeNumber = (a: number) => {
   if (a % 1 === 0) {
@@ -110,6 +163,11 @@ console.log(wholeNumber(1.123));
 console.log(wholeNumber(1048));
 console.log(wholeNumber(10.48));
 
+// Write a function that takes two numbers (a and b) as arguments
+// If a is smaller than b, divide a by b
+// Otherwise, multiply both numbers
+// Return the resulting value
+
 const isSmaller = (a: number, b: number) => {
   if (a < b) {
     return a / b;
@@ -121,6 +179,11 @@ console.log(isSmaller(10, 100));
 console.log(isSmaller(90, 45));
 console.log(isSmaller(8, 20));
 console.log(isSmaller(2, 0.5));
+
+// Write a function that takes two strings (a and b) as arguments
+// If a contains b, append b to the beginning of a
+// If not, append it to the end
+// Return the concatenation
 
 const containsString = (a: string, b: string) => {
   if (a.includes(b)) {
@@ -134,12 +197,20 @@ console.log(containsString('lips', 's'));
 console.log(containsString('Java', 'script'));
 console.log(containsString(' think, therefore I am', 'I'));
 
+// Write a function that takes a number (a) as argument
+// Round a to the 2nd digit after the comma
+// Return the rounded number
+
 const roundNumber = (a: number) => Math.round(a * 100) / 100;
 
 console.log(roundNumber(2.12397));
 console.log(roundNumber(3.136));
 console.log(roundNumber(1.12397));
 console.log(roundNumber(26.1379));
+
+// Write a function that takes a number (a) as argument
+// Split a into its individual digits and return them in an array
+// Tip: you might want to change the type of the number for the splitting
 
 const splitNumber = (a: number) => {
   const b = a.toString();
@@ -149,6 +220,12 @@ const splitNumber = (a: number) => {
 console.log(splitNumber(10));
 console.log(splitNumber(91));
 console.log(splitNumber(193278));
+
+// It seems like something happened to these strings
+// Can you figure out how to clear up the chaos?
+// Write a function that joins these strings together such that they form the following words:
+// 'Javascript', 'Countryside', and 'Downtown'
+// You might want to apply basic JS string methods such as replace(), split(), slice() etc.
 
 const replaceString = (a: string, b: string) => {
   const splitString = b.split('');
@@ -161,6 +238,11 @@ const replaceString = (a: string, b: string) => {
 console.log(replaceString('java', 'tpi%rcs'));
 console.log(replaceString('c%ountry', 'edis'));
 console.log(replaceString('down', 'nw%ot'));
+
+// This challenge is a little bit more complex
+// Write a function that takes a number (a) as argument
+// If a is prime, return a
+// If not, return the next higher prime number
 
 const primeNumbers = (a: number) => {
   if (a <= 1) return false;
@@ -194,6 +276,11 @@ console.log(findAnyNumber(7));
 console.log(findAnyNumber(127));
 console.log(findAnyNumber(2000));
 
+// Write a function that takes two numbers, say x and y, as arguments
+// Check if x is divisible by y
+// If yes, return x
+// If not, return the next higher natural number that is divisible by y
+
 const isDivisable = (x: number, y: number) => {
   while (x % y !== 0) {
     x++;
@@ -206,11 +293,20 @@ console.log(isDivisable(23, 23));
 console.log(isDivisable(7, 3));
 console.log(isDivisable(-5, 7));
 
+// Write a function that takes two strings (a and b) as arguments
+// Beginning at the end of 'a', insert 'b' after every 3rd character of 'a'
+// Return the resulting string
+
 const twoString = (a: string, b: string) => a.replace(/(?=(...)+$)/g, b);
 
 console.log(twoString('1234567', '.'));
 console.log(twoString('abcde', '$'));
 console.log(twoString('zxyzxyzxyzxyzxyz', 'w'));
+
+// Write a function that takes a string as argument
+// As it is, the string has no meaning
+// Increment each letter to the next letter in the alphabet
+// Return the correct word
 
 const nextLetter = (a: string) => {
   let result = '';
@@ -228,17 +324,28 @@ console.log(nextLetter('bnchmf'));
 console.log(nextLetter('bgddrd'));
 console.log(nextLetter('sdrshmf'));
 
+// Write a function that takes an array (a) and a value (n) as argument
+// Return the nth element of 'a'
+
 const arrayReturn = (a: number[], b: number) => a[b - 1];
 
 console.log(arrayReturn([1, 2, 3, 4, 5], 3));
 console.log(arrayReturn([10, 9, 8, 7, 6], 5));
 console.log(arrayReturn([7, 2, 1, 6, 3], 1));
 
+// Write a function that takes an array (a) as argument
+// Remove the first 3 elements of 'a'
+// Return the result
+
 const removeArray = (a: number[]) => a.slice(3);
 
 console.log(removeArray([1, 2, 3, 4]));
 console.log(removeArray([5, 4, 3, 2, 1, 0]));
 console.log(removeArray([99, 1, 1]));
+
+// Write a function that takes an array (a) as argument
+// Extract the last 3 elements of a
+// Return the resulting array
 
 const lastThreeArray = (a: number[]) => {
   const secondArray = a.slice(-3);
@@ -249,6 +356,10 @@ console.log(lastThreeArray([1, 2, 3, 4]));
 console.log(lastThreeArray([5, 4, 3, 2, 1, 0]));
 console.log(lastThreeArray([99, 1, 1]));
 
+// Write a function that takes an array (a) as argument
+// Extract the first 3 elements of a
+// Return the resulting array
+
 const firstThreeArray = (a: number[]) => {
   const secondArray = a.slice(0, 3);
   return secondArray;
@@ -258,6 +369,9 @@ console.log(firstThreeArray([1, 2, 3, 4]));
 console.log(firstThreeArray([5, 4, 3, 2, 1, 0]));
 console.log(firstThreeArray([99, 1, 1]));
 
+// Write a function that takes an array (a) and a number (n) as arguments
+// It should return the last n elements of a
+
 const lastElements = (a: number[], n: number) => {
   const secondArray = a.slice(-n);
   return secondArray;
@@ -266,6 +380,10 @@ const lastElements = (a: number[], n: number) => {
 console.log(lastElements([1, 2, 3, 4, 5], 2));
 console.log(lastElements([1, 2, 3], 6));
 console.log(lastElements([1, 2, 3, 4, 5, 6, 7, 8], 3));
+
+// Write a function that takes an array (a) and a value (b) as argument
+// The function should clean a from all occurrences of b
+// Return the filtered array
 
 const clearArray = (a: any, b: any) => {
   const unfilteredArray = a.filter((number: any) => number !== b);
@@ -277,11 +395,17 @@ console.log(clearArray([1, 2, '2'], '2'));
 console.log(clearArray([false, '2', 1], false));
 console.log(clearArray([1, 2, '2', 1], 1));
 
+// Write a function that takes an array (a) as argument
+// Return the number of elements in a
+
 const numberOfElementsArray = (a: number[]) => a.length;
 
 console.log(numberOfElementsArray([1, 2, 2, 4]));
 console.log(numberOfElementsArray([9, 9, 9]));
 console.log(numberOfElementsArray([4, 3, 2, 1, 0]));
+
+// Write a function that takes an array of numbers as argument
+// Return the number of negative values in the array
 
 const negativeValues = (a: number[]) => {
   const negativesCount = a.filter((value) => value < 0);
@@ -292,21 +416,34 @@ console.log(negativeValues([1, -2, 2, -4]));
 console.log(negativeValues([0, 9, 1]));
 console.log(negativeValues([4, -3, 2, 1, 0]));
 
+// Write a function that takes an array of numbers as argument
+// It should return an array with the numbers sorted in descending order
+
 const sortArray = (a: number[]) => a.sort().reverse();
 
 console.log(sortArray([1, 3, 2]));
 console.log(sortArray([4, 2, 3, 1]));
+
+// Write a function that takes an array of strings as argument
+// Sort the array elements alphabetically
+// Return the result
 
 const stringSortArray = (a: string[]) => a.sort();
 
 console.log(stringSortArray(['b', 'c', 'd', 'a']));
 console.log(stringSortArray(['z', 'c', 'd', 'a', 'y', 'a', 'w']));
 
+// Write a function that takes an array of numbers as argument
+// It should return the average of the numbers
+
 const averageNumbersArray = (a: number[]) => a.reduce((a, b) => a + b, 0) / a.length;
 
 console.log(averageNumbersArray([10, 100, 40]));
 console.log(averageNumbersArray([10, 100, 1000]));
 console.log(averageNumbersArray([-50, 0, 50, 200]));
+
+// Write a function that takes an array of strings as argument
+// Return the longest string
 
 const longestString = (a: string[]) => {
   let longest = '';
@@ -320,6 +457,10 @@ const longestString = (a: string[]) => {
 
 console.log(longestString(['help', 'me']));
 console.log(longestString(['I', 'need', 'candy']));
+
+// Write a function that takes an array as argument
+// It should return true if all elements in the array are equal
+// It should return false otherwise
 
 const equalElements = (a: any[]) => {
   const referenceElements = a[0];
@@ -336,17 +477,29 @@ console.log(equalElements(['test', 'test', 'test']));
 console.log(equalElements([1, 1, 1, 2]));
 console.log(equalElements(['10', 10, 10, 10]));
 
+// Write a function that takes arguments an arbitrary number of arrays
+// It should return an array containing the values of all arrays
+
 const multipleArrays = (...arrays: any[]) => [].concat(...arrays);
 
 console.log(multipleArrays([1, 2, 3], [4, 5, 6]));
 console.log(multipleArrays(['a', 'b', 'c'], [4, 5, 6]));
 console.log(multipleArrays([true, true], [1, 2], ['a', 'b']));
 
+// Write a function that takes an array of objects as argument
+// Sort the array by property b in ascending order
+// Return the sorted array
+
 const arrayOfObjects = (arr: any[]) => arr.sort((a, b) => a.a - b.b);
 
 console.log(arrayOfObjects([{ a: 1, b: 2 }, { a: 5, b: 4 }]));
 console.log(arrayOfObjects([{ a: 2, b: 10 }, { a: 5, b: 4 }]));
 console.log(arrayOfObjects([{ a: 1, b: 7 }, { a: 2, b: 1 }]));
+
+// Write a function that takes two arrays as arguments
+// Merge both arrays and remove duplicate values
+// Sort the merge result in ascending order
+// Return the resulting array
 
 const removeDublicates = (a: number[], b: number[]) => {
   const mergedArrays = a.concat(b);
@@ -363,6 +516,10 @@ const removeDublicates = (a: number[], b: number[]) => {
 console.log(removeDublicates([1, 2, 3], [3, 4, 5]));
 console.log(removeDublicates([-10, 22, 333, 42], [-11, 5, 22, 41, 42]));
 
+// Write a function that takes an array (a) and a number (b) as arguments
+// Sum up all array elements with a value greater than b
+// Return the sum
+
 const sumElements = (a: number[], b: number) => {
   let result = 0;
   for (let i = 0; i < a.length; i++) {
@@ -377,6 +534,9 @@ console.log(sumElements([1, 2, 3, 4, 5, 6, 7], 2));
 console.log(sumElements([-10, -11, -3, 1, -4], -3));
 console.log(sumElements([78, 99, 100, 101, 401], 99));
 
+// Write a function that takes two numbers (min and max) as arguments
+// Return an array of numbers in the range min to max
+
 const minMax = (min: number, max: number) => {
   const result = [];
   for (let i = min; i <= max; i++) {
@@ -389,6 +549,13 @@ console.log(minMax(2, 10));
 console.log(minMax(1, 3));
 console.log(minMax(-5, 5));
 console.log(minMax(2, 7));
+
+// Write a function that takes an array of strings as argument
+// Group those strings by their first letter
+// Return an object that contains properties with keys representing first letters
+// The values should be arrays of strings containing only the corresponding strings
+// For example, the array ['Alf', 'Alice', 'Ben'] should be transformed to
+// { a: ['Alf', 'Alice'], b: ['Ben']}
 
 const fristLetterGroup = (arr: string[]) => {
   const firstObejct: Record<string, string[]> = {};
@@ -406,6 +573,12 @@ console.log(fristLetterGroup(['Alf', 'Alice', 'Ben']));
 console.log(fristLetterGroup(['Ant', 'Bear', 'Bird']));
 console.log(fristLetterGroup(['Berlin', 'Paris', 'Prague']));
 
+// Write a function that takes an array with arbitrary elements and a number as arguments
+// Return a new array, the first element should be either the given number itself
+// or zero if the number is smaller than 6
+// The other elements should be the elements of the original array
+// Try not to mutate the original array
+
 const elementsArray = (arr: any[], n: number) => {
   const result = [];
   if (n >= 6) {
@@ -420,6 +593,10 @@ console.log(elementsArray([1, 2, 3], 6));
 console.log(elementsArray(['a', 'b'], 2));
 console.log(elementsArray([null, false], 11));
 
+// Write a function that takes an array (a) and a value (n) as arguments
+// Save every nth element in a new array
+// Return the new array
+
 const nthArray = (a: number[], n: number) => {
   const result = [];
   for (let i = -1; i < a.length; i += n) {
@@ -432,6 +609,9 @@ const nthArray = (a: number[], n: number) => {
 console.log(nthArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
 console.log(nthArray([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 5));
 console.log(nthArray([7, 2, 1, 6, 3, 4, 5, 8, 9, 10], 2));
+
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key country
 
 const objectCountry = (obj: {country: string, continent: string}) => obj.country;
 
